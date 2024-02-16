9e98cffdc708f11aa52b102fc025002c9e851df3
Nishan-Pradhan06 <nishanpradhan500@gmail.com>
2024-02-16 09:58:17 +0545

    arry_implementation_stack_Push_pop_peek

---
+++ array_implementation.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <conio.h>
+#define MAX 10
+#define TRUE 1
+#define FALSE 0
+struct stack
+{
+    int tos;        // top of the stack
+    int items[MAX]; // array is used as storehouse of stack
+};
+
+int isEmplty(struct stack *s) // underflow test
+{
+    // return s-> tos =-1;
+    if (s->tos == -1)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+int isFull(struct stack *s) // overflow test
+{
+    // return s->tos==MAX-1;
+    if (s->tos == MAX - 1)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+int push(struct stack *s, int num)
+{
+    if (isFull(s)) // if stack if full then dont push
+    {
+        return FALSE; // return fa;se tp indicate that push has failed.
+    }
+    else
+    {
+        s->tos++;
+        s->items[s->tos] = num;
+        return TRUE; // return ture indicating the sucess of push
+    }
+}
+
+int pop(struct stack *s)
+{
+    // if (isEmplty(s)) // stack underlow
+    // {
+    //     return TRUE; // pop failed
+    // }
+    // else
+    // {
+    //     s->tos--;
+    //     return TRUE; // pop sucess
+    // }
+
+    // int num = s->items[s->tos];
+    // s->tos--;
+    // return num; //
+
+    return s->items[s->tos--];
+}
+
+int peek(struct stack *s)
+{
+    return s->items[s->tos];
+}
+
+void display(struct stack *s)
+{
+    int i;
+    if (isEmplty(s))
+    {
+        printf("\nStack is Empty.");
+    }
+    else
+    {
+        printf("\nStack contains:\n");
+        for (i = s->tos; i >= 0; i++)
+        {
+            /* code */
+        }
+    }
+}
+
+void main()
+{
+    struct stack s;
+    char choice;
+    int num;
+    s.tos = -1; // empty stack initialized
+    do
+    {
+        printf("Select an option :\n1.push\n2.pop\n3.List\n4.Exit");
+        fflush(stdin);
+        choice = getche();
+        switch (choice)
+        {
+        case '1':
+            printf("Enter a number: ");
+            scanf("%d", num);
+            if (push(&s, num))
+            {
+                printf("Item pushed into the stack sucessfully");
+            }
+            else
+            {
+                printf("Stack overflow. Pop some items before push.");
+            }
+            break;
+        case '2':
+
+            if (isEmplty(&s))
+            {
+                printf("Stack overflow. Pop some items before push.");
+            }
+            else
+            {
+                num = peek(&s);
+                pop(&s);
+                printf("%d was poped from the stack", pop(&s));
+            }
+            break;
+        case '3':
+            display(&s);
+            break;
+        case '4':
+            printf("Sorry to see you go. ");
+
+            break;
+        default:
+            break;
+        }
+    } while (TRUE);
+}
